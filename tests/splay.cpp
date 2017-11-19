12f776672ab2ff2daea66d24bbd82b99fbea09d2
BinaryTreesImplementation <33779350+BinaryTreesImplementation@users.noreply.github.com>
2017-11-19 15:22:28 +0300

    Update splay.cpp

--- tests/splay.cpp
+++ tests/splay.cpp
@@ -3,9 +3,9 @@
 #include "treap.h"
 
 TEST_CASE("insert_root", "[root]") {
-   SplayTree<int> testSplayTree;
-   testSplayTree.insert(10);
-   REQUIRE(testSplayTree.search(10) == 1);
+   Treap<int> TreapTree;
+   TreapTree.insert(10);
+   REQUIRE(TreapTree.search(10) != nullptr);
 }
 
 
